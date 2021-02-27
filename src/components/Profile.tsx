fb13fca7ab31b4fd87b873702a85188061a693c9
MiDrAjX <jeisommarques78@gmail.com>
2021-02-27 01:23:20 -0300

    Primeiro Commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/MiDrAjX.png" alt="Jeison Marques" />
+            <div>
+                <strong>Jeison Marques</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
